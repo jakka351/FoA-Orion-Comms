1f7d1dd917f4f33c80a56383240eb75d1cf77681
Jack Leighton <jakka351@outlook.com>
2024-07-03 18:00:24 +1000

    Create license.cs

---
+++ license.cs
@@ -0,0 +1,63 @@
+#region Copyright (c) 2024, Jack Leighton
+// /////     __________________________________________________________________________________________________________________
+// /////
+// /////                  __                   __              __________                                      __   
+// /////                _/  |_  ____   _______/  |_  __________\______   \_______   ____   ______ ____   _____/  |_ 
+// /////                \   __\/ __ \ /  ___/\   __\/ __ \_  __ \     ___/\_  __ \_/ __ \ /  ___// __ \ /    \   __\
+// /////                 |  | \  ___/ \___ \  |  | \  ___/|  | \/    |     |  | \/\  ___/ \___ \\  ___/|   |  \  |  
+// /////                 |__|  \___  >____  > |__|  \___  >__|  |____|     |__|    \___  >____  >\___  >___|  /__|  
+// /////                           \/     \/            \/                             \/     \/     \/     \/      
+// /////                                                          .__       .__  .__          __                    
+// /////                               ____________   ____   ____ |__|____  |  | |__| _______/  |_                  
+// /////                              /  ___/\____ \_/ __ \_/ ___\|  \__  \ |  | |  |/  ___/\   __\                 
+// /////                              \___ \ |  |_> >  ___/\  \___|  |/ __ \|  |_|  |\___ \  |  |                   
+// /////                             /____  >|   __/ \___  >\___  >__(____  /____/__/____  > |__|                   
+// /////                                  \/ |__|        \/     \/        \/             \/                         
+// /////                                  __                         __  .__                                        
+// /////                   _____   __ ___/  |_  ____   _____   _____/  |_|__|__  __ ____                            
+// /////                   \__  \ |  |  \   __\/  _ \ /     \ /  _ \   __\  \  \/ // __ \                           
+// /////                    / __ \|  |  /|  | (  <_> )  Y Y  (  <_> )  | |  |\   /\  ___/                           
+// /////                   (____  /____/ |__|  \____/|__|_|  /\____/|__| |__| \_/  \___  >                          
+// /////                        \/                         \/                          \/                           
+// /////                                                  .__          __  .__                                      
+// /////                                       __________ |  |  __ ___/  |_|__| ____   ____   ______                
+// /////                                      /  ___/  _ \|  | |  |  \   __\  |/  _ \ /    \ /  ___/                
+// /////                                      \___ (  <_> )  |_|  |  /|  | |  (  <_> )   |  \\___ \                 
+// /////                                     /____  >____/|____/____/ |__| |__|\____/|___|  /____  >                
+// /////                                          \/                                      \/     \/                 
+// /////                                   Tester Present Specialist Automotive Solutions
+// /////     __________________________________________________________________________________________________________________
+// /////      |--------------------------------------------------------------------------------------------------------------|
+// /////      |       https://github.com/jakka351/| https://testerPresent.com.au | https://facebook.com/testerPresent        |
+// /////      |--------------------------------------------------------------------------------------------------------------|
+// /////      | Copyright (c) 2022/2023/2024 Benjamin Jack Leighton                                                          |          
+// /////      | All rights reserved.                                                                                         |
+// /////      |--------------------------------------------------------------------------------------------------------------|
+// /////        Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// /////        the following conditions are met:
+// /////        1.    With the express written consent of the copyright holder.
+// /////        2.    Redistributions of source code must retain the above copyright notice, this
+// /////              list of conditions and the following disclaimer.
+// /////        3.    Redistributions in binary form must reproduce the above copyright notice, this
+// /////              list of conditions and the following disclaimer in the documentation and/or other
+// /////              materials provided with the distribution.
+// /////        4.    Neither the name of the organization nor the names of its contributors may be used to
+// /////              endorse or promote products derived from this software without specific prior written permission.
+// /////      _________________________________________________________________________________________________________________
+// /////      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// /////      INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// /////      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// /////      SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// /////      SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// /////      WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// /////      USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// /////      _________________________________________________________________________________________________________________
+// /////
+// /////       This software can only be distributed with my written permission. It is for my own educational purposes and  
+// /////       is potentially dangerous to ECU health and safety. Gracias a Gato Blancoford desde las alturas del mar de chelle.                                                        
+// /////      _________________________________________________________________________________________________________________
+// /////
+// /////
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion License
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
